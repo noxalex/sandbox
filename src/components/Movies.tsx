d35e7727e1a69daab6ddf808d4d51a5232bddfd4
Панько Александр Иванович <alexander.panko@gost-group.com>
2018-09-05 19:04:06 +0300

    filter

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
 import MovieCard from './MovieCard';
+import styled from 'styled-components';
 import data from '../data';
 
-const Movies = () => {
-  return (
-    <div className="movies">
-      {data.shows.map(show => (
-        <MovieCard key={show.imdbID} {...show} />
-      ))}
-    </div>
-  );
-};
+const Input = styled.input`
+  box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.12);
+`;
+
+class Movies extends React.Component {
+  state = {
+    searchString: 'black'
+  };
+
+  render() {
+    const filtered = data.shows.filter(show =>
+      `${show.title} ${show.description}`
+        .toLocaleUpperCase()
+        .indexOf(this.state.searchString.toUpperCase())
+    );
+
+    console.log(filtered);
+
+    return (
+      <div className="movies">
+        <Input type="text" placeholder="search for a movie you like" />
+        {data.shows.map(show => (
+          <MovieCard key={show.imdbID} {...show} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Movies;
